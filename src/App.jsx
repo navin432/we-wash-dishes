99abf09b130070565b240fa8fff250bac9058baf
Navin <navin20604@gmail.com>
2025-07-05 17:17:23 -0400

    Build a layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import { Button } from "@chakra-ui/react";
 function App() {
-  return <Button colorScheme="blue">We Wash Dishes</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "main main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
